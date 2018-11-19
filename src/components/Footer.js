f5afdd4b69c9ce25d4e3176d15d3e39f07b844bf
William Chandler <chndlr.wllm@gmail.com>
2018-11-18 20:28:06 -0600

    new colors, services descriptions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,9 +23,9 @@ const Footer = (props) => (
                 <dd>Austin, Texas</dd>
             </dl>
             <ul className="icons">
-                <li><a href="#" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon fa-facebook alt"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon fa-instagram alt"><span className="label">Instagram</span></a></li>
+                {/* <li><a href="#" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li> */}
+                <li><a href="https://www.instagram.com/jchand_doula/" target=
+                '_blank' className="icon fa-instagram alt"><span className="label">Instagram</span></a></li>
             </ul>
         </section>
         <p className="copyright">&copy; BirthWorks ATX</p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import logo from '../assets/images/logo.svg';
 const Header = (props) => (
     <header id="header" className="alt">
         {/* <span className="logo"><img src={logo} alt="" /></span> */}
+        <span style={{fontSize:'60px', marginBottom:'10px', color:'#fff'}} className="icon logo fa-leaf"></span>
         <h1>Hi, I'm Jacqueline</h1>
         <p>a certified birth and postpartum doula <br/>and compassionate educator <br/> based in Austin</p>
     </header>
